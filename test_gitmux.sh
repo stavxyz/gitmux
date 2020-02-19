8b30c992fe626a8af87dbd6a0179b5fb77897182
Sam Stavinoha <smlstvnh@gmail.com>
2020-02-18 22:44:59 -0600

    rename to gitmux

--- test_gitmux.sh
+++ test_gitmux.sh
@@ -33,7 +33,7 @@ export GITHUB_HOST=${GITHUB_HOST:-'github.com'}
 export GITHUB_OWNER=${GITHUB_OWNER:-samstav}
 
 
-TMPTESTWORKDIR=$(mktemp -t 'repoman-test-XXXX' -d || errxit "Failed to create tmpdir.")
+TMPTESTWORKDIR=$(mktemp -t 'gitmux-test-XXXX' -d || errxit "Failed to create tmpdir.")
 echo "Working in tmpdir ${TMPTESTWORKDIR}"
 _pushd "${TMPTESTWORKDIR}"
 
@@ -89,11 +89,11 @@ createRepository() {
   # `hub create` must be run from inside a git repository. (weird)
   log "hub is creating your new repository now!"
   # hub create [-poc] [-d DESCRIPTION] [-h HOMEPAGE] [[ORGANIZATION/]NAME]
-  TMPHUBCREATEWORKDIR=$(mktemp -t 'repoman-tests-XXXX' -d || errxit "Failed to create tmpdir.")
+  TMPHUBCREATEWORKDIR=$(mktemp -t 'gitmux-tests-XXXX' -d || errxit "Failed to create tmpdir.")
   _pushd "${TMPHUBCREATEWORKDIR}" && git init --quiet
-  NEW_REPOSITORY_DESCRIPTION="Test repository for repoman. If you find this lingering you may safely delete this repository."
+  NEW_REPOSITORY_DESCRIPTION="Test repository for gitmux. If you find this lingering you may safely delete this repository."
   hub create ${_hubcreateopts:-} --remote-name hello -d "${NEW_REPOSITORY_DESCRIPTION}" "${_owner}/${_project}"
-  git commit --message 'Hello: this repository was created by repoman.' --allow-empty
+  git commit --message 'Hello: this repository was created by gitmux.' --allow-empty
   git push hello "master:master"
   _popd
   log "cleaning up hub-create workdir"
@@ -105,7 +105,7 @@ createRepository() {
 #####################################
 #### Setup source git repository.
 #####################################
-SOURCE_REPOSITORY_NAME="repoman_test_source_$(rands 8)"
+SOURCE_REPOSITORY_NAME="gitmux_test_source_$(rands 8)"
 mkdir -p "${SOURCE_REPOSITORY_NAME}"
 _pushd "${SOURCE_REPOSITORY_NAME}" && SOURCE_REPOSITORY_PATH="$(pwd)"
 git init
@@ -117,7 +117,7 @@ git checkout -b something-new --track source_remote_name/master
 echo "Hello World" > "hello.txt"
 echo "## wat" > "wat.md"
 git add "hello.txt"
-git commit -m 'initial source repo commit: repoman test'
+git commit -m 'initial source repo commit: gitmux test'
 git add "wat.md"
 git commit -m 'and now wat?'
 _sha=$(git rev-parse --short HEAD)
@@ -126,7 +126,7 @@ _popd
 #####################################
 #### Setup destination git repository.
 #####################################
-DESTINATION_REPOSITORY_NAME="repoman_test_destination_$(rands 8)"
+DESTINATION_REPOSITORY_NAME="gitmux_test_destination_$(rands 8)"
 mkdir -p "${DESTINATION_REPOSITORY_NAME}"
 _pushd "${DESTINATION_REPOSITORY_NAME}"
 DESTINATION_REPOSITORY_PATH="$(pwd)"
@@ -139,7 +139,7 @@ git fetch --update-head-ok destination_remote_name
 git checkout master
 # Now back to current branch.
 git checkout -b destination_current_branch --track destination_remote_name/master
-git commit --allow-empty -m 'initial destination repo commit: repoman test'
+git commit --allow-empty -m 'initial destination repo commit: gitmux test'
 _popd && _popd
 
 
@@ -155,7 +155,7 @@ echo
 ##########################################
 
 test_defaults_with_existing_upstream_destination() {
-  ./repoman.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}"
+  ./gitmux.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}"
   _pushd "${DESTINATION_REPOSITORY_PATH}"
   git checkout "update-from-something-new-${_sha}-rebase-strategy-ours"
   local output=''
@@ -181,7 +181,7 @@ echo
 ##########################################
 
 test_rebase_strategy_theirs_with_existing_upstream_destination() {
-  ./repoman.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}" -p place_content_in_this_subdir -b master -X theirs
+  ./gitmux.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}" -p place_content_in_this_subdir -b master -X theirs
   _pushd "${DESTINATION_REPOSITORY_PATH}"
   git checkout "update-from-something-new-${_sha}-rebase-strategy-theirs"
   local output=''
@@ -200,15 +200,15 @@ echo
 ##########################################
 #### Test 3:
 ####    - defaults with -c (create repo for me)
-####    - repoman should create repository for me
+####    - gitmux should create repository for me
 ####    - rebase strategy 'ours'
 ##########################################
 
 test_defaults_destination_dne_yet() {
-  NEW_REPO_URI="${GITHUB_OWNER}/repoman_test_destination_$(rands 8)"
+  NEW_REPO_URI="${GITHUB_OWNER}/gitmux_test_destination_$(rands 8)"
   repositoriesToDelete+=("${NEW_REPO_URI}")
   NEW_REPO_NO_UPSTREAM_YET="git@${GITHUB_HOST}:${NEW_REPO_URI}.git"
-  ./repoman.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}"
+  ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}"
   _pushd "${DESTINATION_REPOSITORY_PATH}"
   git checkout "update-from-something-new-${_sha}-rebase-strategy-ours"
   local output=''
@@ -229,17 +229,17 @@ echo
 ##########################################
 #### Test 4:
 ####    - defaults with -c (create repo for me)
-####    - repoman should create repository for me
+####    - gitmux should create repository for me
 ####    - rebase strategy 'ours'
 ####    - add github team infraconfig/infracore
 ##########################################
 
 test_defaults_add_orgteam() {
-  NEW_REPO_PROJECT_NAME="repoman_test_destination_$(rands 8)"
+  NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands 8)"
   repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
   NEW_REPO_NO_UPSTREAM_YET="git@${GITHUB_HOST}:${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
-  ./repoman.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -z infraconfig/infracore
-  log "Now cloning repository which should have been created on GitHub by repoman."
+  ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -z infraconfig/infracore
+  log "Now cloning repository which should have been created on GitHub by gitmux."
   git clone "${NEW_REPO_NO_UPSTREAM_YET}"
   # This should create a directory called $NEW_REPO_PROJECT_NAME
   _pushd "${NEW_REPO_PROJECT_NAME}"
@@ -263,17 +263,17 @@ echo
 ##########################################
 #### Test 5:
 ####    - defaults with -c (create repo for me)
-####    - repoman should create repository for me
+####    - gitmux should create repository for me
 ####    - rebase strategy 'ours'
 ####    - selective file migration
 ##########################################
 
 test_defaults_destination_dne_yet_only_wat() {
-  NEW_REPO_PROJECT_NAME="repoman_test_destination_$(rands 8)"
+  NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands 8)"
   repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
   NEW_REPO_NO_UPSTREAM_YET="git@${GITHUB_HOST}:${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
-  ./repoman.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -l "-- wat.md"
-  log "Now cloning repository which should have been created on GitHub by repoman."
+  ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -l "-- wat.md"
+  log "Now cloning repository which should have been created on GitHub by gitmux."
   git clone "${NEW_REPO_NO_UPSTREAM_YET}"
   # This should create a directory called $NEW_REPO_PROJECT_NAME
   _pushd "${NEW_REPO_PROJECT_NAME}"
