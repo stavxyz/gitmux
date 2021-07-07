076bd8020ae0f7ee60847269bfc174e646d24c5a
Sam Stavinoha <smlstvnh@gmail.com>
2021-07-06 20:42:47 -0500

    use 'trunk' instead of 'master'

--- test_gitmux.sh
+++ test_gitmux.sh
@@ -80,6 +80,9 @@ rands() {
   echo $RANDOM$RANDOM | tr '0-9' '[:lower:]'
 }
 
+REPO_REGEX='s/(.*:\/\/|^git@)(.*)([\/:]{1})([a-zA-Z0-9_\.-]{1,})([\/]{1})([a-zA-Z0-9_\.-]{1,}$)'
+
+
 createRepository() {
   local _owner="${1}"
   local _project="${2}"
@@ -103,15 +106,25 @@ createRepository() {
   _pushd "${TMPGHCREATEWORKDIR}"
   NEW_REPOSITORY_DESCRIPTION="Test repository for gitmux. If you find this lingering you may safely delete this repository."
   log "gh-cli is creating your new repository now!"
-  gh repo create "${_owner}/${_project}" ${_ghcreateopts:-} --confirm --description "${NEW_REPOSITORY_DESCRIPTION}"
+  gh repo create "${_owner}/${_project}" ${_ghcreateopts:-} --license=unlicense --gitignore 'VVVV' --confirm --description "${NEW_REPOSITORY_DESCRIPTION}"
+  pushd ${_project}
   log "renaming origin to hello"
   git remote rename origin hello
+  pwd
+
+  #_new_url=$(git remote get-url hello | sed -E "${REPO_REGEX}""/https\:\/\/${GH_TOKEN}\@\2\/\4\/\6/")
+  _new_url=$(git remote get-url hello | sed -E "${REPO_REGEX}""/https\:\/\/git\:${GH_TOKEN}\@\2\/\4\/\6/")
+  log "new url: ${_new_url}"
+  git remote set-url hello ${_new_url}
+
   git commit --message 'Hello: this repository was created by gitmux.' --allow-empty
   git remote --verbose show
   log "pushing change to hello"
-  git push hello "master:master"
-  _popd
-  log "cleaning up gh-create-repo workdir"
+  git push hello "trunk:trunk"
+  pwd
+  _popd && _popd
+  pwd
+  log "cleaning up gh-create-repo workdir --> ${TMPGHCREATEWORKDIR}"
   rm -rf "${TMPGHCREATEWORKDIR}"
   ########## </GH CREATE REPO> ################
 }
@@ -123,12 +136,13 @@ createRepository() {
 SOURCE_REPOSITORY_NAME="gitmux_test_source_$(rands)"
 mkdir -p "${SOURCE_REPOSITORY_NAME}"
 _pushd "${SOURCE_REPOSITORY_NAME}" && SOURCE_REPOSITORY_PATH="$(pwd)"
-git init
+git init --initial-branch=trunk
 createRepository "${GITHUB_OWNER}" "${SOURCE_REPOSITORY_NAME}"
 repositoriesToDelete+=("${GITHUB_OWNER}/${SOURCE_REPOSITORY_NAME}")
-git remote add source_remote_name "git@${GH_HOST}:${GITHUB_OWNER}/${SOURCE_REPOSITORY_NAME}.git"
+git remote add source_remote_name "https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${SOURCE_REPOSITORY_NAME}.git"
+log "Fetching in $(pwd)"
 git fetch source_remote_name
-git checkout -b something-new --track source_remote_name/master
+git checkout -b something-new --track source_remote_name/trunk
 echo "Hello World" > "hello.txt"
 echo "## wat" > 'wat.md'
 mkdir -p toto
@@ -146,19 +160,19 @@ _popd
 #####################################
 #### Setup destination git repository.
 #####################################
-DESTINATION_REPOSITORY_NAME="gitmux_test_destination_$(rands 8)"
+DESTINATION_REPOSITORY_NAME="gitmux_test_destination_$(rands)"
 mkdir -p "${DESTINATION_REPOSITORY_NAME}"
 _pushd "${DESTINATION_REPOSITORY_NAME}"
 DESTINATION_REPOSITORY_PATH="$(pwd)"
-git init
+git init --initial-branch=trunk
 createRepository "${GITHUB_OWNER}" "${DESTINATION_REPOSITORY_NAME}"
 repositoriesToDelete+=("${GITHUB_OWNER}/${DESTINATION_REPOSITORY_NAME}")
-git remote add destination_remote_name "git@${GH_HOST}:${GITHUB_OWNER}/${DESTINATION_REPOSITORY_NAME}.git"
+git remote add destination_remote_name "https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${DESTINATION_REPOSITORY_NAME}.git"
 git fetch --update-head-ok destination_remote_name
-# This actually creates a local 'master' tracking branch.
-git checkout master
+# This actually creates a local 'trunk' tracking branch.
+git checkout trunk
 # Now back to current branch.
-git checkout -b destination_current_branch --track destination_remote_name/master
+git checkout -b destination_current_branch --track destination_remote_name/trunk
 git commit --allow-empty -m 'initial destination repo commit: gitmux test'
 _popd && _popd
 
@@ -201,7 +215,7 @@ echo
 ##########################################
 
 test_rebase_strategy_theirs_with_existing_upstream_destination() {
-  ./gitmux.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}" -p place_content_in_this_subdir -b master -X theirs
+  ./gitmux.sh -v -r "${SOURCE_REPOSITORY_PATH}" -t "${DESTINATION_REPOSITORY_PATH}" -p place_content_in_this_subdir -b trunk -X theirs
   _pushd "${DESTINATION_REPOSITORY_PATH}"
   git checkout "update-from-something-new-${_sha}-rebase-strategy-theirs"
   local output=''
@@ -225,11 +239,14 @@ echo
 ##########################################
 
 test_defaults_destination_dne_yet() {
-  NEW_REPO_URI="${GITHUB_OWNER}/gitmux_test_destination_$(rands 8)"
-  repositoriesToDelete+=("${NEW_REPO_URI}")
-  NEW_REPO_NO_UPSTREAM_YET="git@${GH_HOST}:${NEW_REPO_URI}.git"
+  NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands)"
+  repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
+  NEW_REPO_NO_UPSTREAM_YET="https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
   ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}"
-  _pushd "${DESTINATION_REPOSITORY_PATH}"
+  log "Now cloning repository which should have been created on GitHub by gitmux."
+  git clone "${NEW_REPO_NO_UPSTREAM_YET}"
+  # This should create a directory called $NEW_REPO_PROJECT_NAME
+  _pushd "${NEW_REPO_PROJECT_NAME}"
   git checkout "update-from-something-new-${_sha}-rebase-strategy-ours"
   local output=''
   if output=$(cat hello.txt) && [ "${output}" == "Hello World" ];then
@@ -257,7 +274,7 @@ echo
 test_defaults_add_orgteam() {
   NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands)"
   repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
-  NEW_REPO_NO_UPSTREAM_YET="git@${GH_HOST}:${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
+  NEW_REPO_NO_UPSTREAM_YET="https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
   ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -z infraconfig/infracore
   log "Now cloning repository which should have been created on GitHub by gitmux."
   git clone "${NEW_REPO_NO_UPSTREAM_YET}"
@@ -291,7 +308,7 @@ echo
 test_defaults_destination_dne_yet_only_wat() {
   NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands)"
   repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
-  NEW_REPO_NO_UPSTREAM_YET="git@${GH_HOST}:${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
+  NEW_REPO_NO_UPSTREAM_YET="https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
   ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -l "wat.md"
   log "Now cloning repository which should have been created on GitHub by gitmux."
   git clone "${NEW_REPO_NO_UPSTREAM_YET}"
@@ -318,7 +335,7 @@ test_defaults_destination_dne_yet_only_wat() {
 test_defaults_destination_dne_yet_only_toto() {
   NEW_REPO_PROJECT_NAME="gitmux_test_destination_$(rands)"
   repositoriesToDelete+=("${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}")
-  NEW_REPO_NO_UPSTREAM_YET="git@${GH_HOST}:${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
+  NEW_REPO_NO_UPSTREAM_YET="https://${GITHUB_OWNER}:${GH_TOKEN}@${GH_HOST}/${GITHUB_OWNER}/${NEW_REPO_PROJECT_NAME}.git"
   ./gitmux.sh -v -c -r "${SOURCE_REPOSITORY_PATH}" -t "${NEW_REPO_NO_UPSTREAM_YET}" -l "toto"
   log "Now cloning repository which should have been created on GitHub by gitmux."
   git clone "${NEW_REPO_NO_UPSTREAM_YET}"
